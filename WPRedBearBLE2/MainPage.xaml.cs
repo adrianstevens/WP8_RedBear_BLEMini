836ef0a533388721088c993ad9b1a17a646689ab
Adrian Stevens <adrian.stevens@hotmail.com>
2015-01-22 08:33:22 -0800

    added 2nd project

--- WPRedBearBLE2/MainPage.xaml.cs
+++ WPRedBearBLE2/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using WPRedBearBLE.Resources;
+using WPRedBearBLE2.Resources;
 using Windows.Devices.Enumeration;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Devices.Bluetooth;
@@ -19,7 +19,7 @@ using Windows.Storage.Streams;
 //RedBear BLE Mini board was connected directly to a CP2102 USB UART and communication was tested using CoolTerm http://freeware.the-meiers.org/
 //Try http://www.themethodology.net for more information
 
-namespace WPRedBearBLE
+namespace WPRedBearBLE2
 {
     public partial class MainPage : PhoneApplicationPage
     {
@@ -36,14 +36,22 @@ namespace WPRedBearBLE
             InitializeComponent();
 
             RedBearConnect();
+
+            btnSendMessage.Click += btnSendMessage_Click;
         }
 
-        async Task RedBearConnect ()
+        void btnSendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(txtMessage.Text);
+            txtMessage.Text = String.Empty;
+        }
+
+        async Task RedBearConnect()
         {
             if (await RefreshDeviceList() == 0)
                 return;
 
-            if(await ConnectToRedBear() == false)
+            if (await ConnectToRedBear() == false)
                 return;
 
             if (FindService() == false)
@@ -52,9 +60,8 @@ namespace WPRedBearBLE
             if (FindCharacteristic() == false)
                 return;
 
-            //send a message        
-            await SendMessage("Hello RedBear!");
-        
+            txtMessage.IsEnabled = true;
+            btnSendMessage.IsEnabled = true;
         }
 
         async Task<int> RefreshDeviceList()
@@ -64,7 +71,7 @@ namespace WPRedBearBLE
                 bleDevices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.GenericAccess));
 
                 OutputMessage("Found " + bleDevices.Count + " device(s)");
-             
+
                 if (bleDevices.Count == 0)
                 {
                     OutputMessage("No BLE Devices found - make sure you've paired your device");
@@ -79,13 +86,13 @@ namespace WPRedBearBLE
             return bleDevices.Count;
         }
 
-        async Task<bool> ConnectToRedBear ()
+        async Task<bool> ConnectToRedBear()
         {
             try
             {
-                for (int i = 0; i < bleDevices.Count; i++ )
+                for (int i = 0; i < bleDevices.Count; i++)
                 {
-                    if(bleDevices[i].Name == "Biscuit" || bleDevices[i].Name == "BLE Mini")
+                    if (bleDevices[i].Name == "Biscuit" || bleDevices[i].Name == "BLE Mini")
                     {
                         _device = await BluetoothLEDevice.FromIdAsync(bleDevices[i].Id);
                         _services = _device.GattServices;
@@ -105,7 +112,7 @@ namespace WPRedBearBLE
             return true;
         }
 
-        bool FindService ()
+        bool FindService()
         {
             foreach (GattDeviceService s in _services)
             {
@@ -115,13 +122,13 @@ namespace WPRedBearBLE
                     OutputMessage("Found Service: " + s.Uuid);
                     return true;
                 }
-                
+
             }
             OutputMessage("Unable to find Biscuit Service 713d0000");
             return false;
         }
 
-        bool FindCharacteristic ()
+        bool FindCharacteristic()
         {
             foreach (var c in _service.GetCharacteristics(new Guid("713d0003-503e-4c75-ba94-3148f18d941e")))
             {
@@ -135,16 +142,15 @@ namespace WPRedBearBLE
             return false;
         }
 
-        async Task<bool> SendMessage (string msg)
+        async Task<bool> SendMessage(string msg)
         {
             DataWriter data = new DataWriter();
             data.WriteString(msg);
 
             var buffer = data.DetachBuffer();
 
-            try 
+            try
             {
-                //first chance exception
                 await _characteristic.WriteValueAsync(buffer, GattWriteOption.WriteWithoutResponse);
 
                 OutputMessage("Sent message: " + msg);
@@ -155,11 +161,9 @@ namespace WPRedBearBLE
                 return false;
             }
             return true;
-
-            
         }
 
-        void OutputMessage (string msg)
+        void OutputMessage(string msg)
         {
             Debug.WriteLine(msg);
             txtOutput.Text += msg + "\r\n";
